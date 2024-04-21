1491036fd60e26fcc80626ca7c98267e5d0a0a14
rabnmgr7 <rabnmgr7@gmail.com>
2024-04-21 09:02:52 +0545

    nginx image deletion option added

--- create-service.sh
+++ create-service.sh
@@ -4,5 +4,6 @@ docker compose down || true
 echo "Removing images"
 docker image rm -f test-result-flask-app:latest || true
 docker image rm -f test-result-mysql-db:latest || true
+docker image rm -f test-result-nginx:latest || true
 echo "Creating docker compose services"
 docker compose up -d
